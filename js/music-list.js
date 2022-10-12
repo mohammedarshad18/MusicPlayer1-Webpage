7f255a0b4918c350a784bbba013fb6bd44e23f50
Mohammed Arshad <81609528+mohammedarshad18@users.noreply.github.com>
2022-10-12 21:42:01 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "I'm Done",
+    artist: "MC Stan",
+    img: "stan1",
+    src: "stan1"
+  },
+  {
+    name: "Snake",
+    artist: "MC Stan",
+    img: "stan2",
+    src: "stan2"
+  },
+  {
+    name: "Kahan Par Hai",
+    artist: "MC Stan",
+    img: "stan3",
+    src: "stan3"
+  },
+  {
+    name: "Broke Is A Joke",
+    artist: "MC Stan",
+    img: "stan4",
+    src: "stan4"
+  },
+  {
+    name: "Nanchaku",
+    artist: "MC Stan",
+    img: "stan5",
+    src: "stan5"
+  },
+  {
+    name: "Rehmani Keeda",
+    artist: "MC Stan",
+    img: "stan6",
+    src: "stan6"
+  },
+];
